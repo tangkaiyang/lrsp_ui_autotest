6ae3ae2feee77cf35b773f831ba0266d3fdd6b05
tangkaiyang <945541696@qq.com>
2019-02-21 16:52:04 +0800

    base.py

---
+++ test_case/models/function.py
@@ -0,0 +1,18 @@
+# -*- coding:UTF-8 -*-
+from selenium import webdriver
+import os
+
+#截图函数
+def insert_img(driver, file_name):
+    base_dir = os.path.dirname(os.path.dirname(__file__))
+    base_dir = str(base_dir)
+    base_dir = base_dir.replace('\\', '/')
+    base = base_dir.split('/test_case/')[0]
+    file_path = base + "/report/image" + file_name
+    driver.get_screenshot_as_file(file_path)
+
+if __name__ == '__main__':
+    driver = webdriver.Chrome()
+    driver.get("http://release.web.beta.lrwanche.com")
+    insert_img(driver, 'lrsp.png')
+    driver.quit()
